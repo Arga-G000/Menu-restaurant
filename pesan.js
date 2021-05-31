e72b9b739af5a5604f1a53d475e6caf2e12491e7
Achmad Arga Bimantara Akbar <83933208+Arga-G000@users.noreply.github.com>
2021-06-01 05:49:28 +0700

    Upload file javascript untuk menu pesan

---
+++ pesan.js
@@ -0,0 +1,389 @@
+function goToMakanan(){
+    document.getElementById("satu").style.display='none';
+    document.getElementById("dua").style.display='block';
+}
+function goToMinuman(){
+    document.getElementById("satu").style.display='none';
+    document.getElementById("tiga").style.display='block';
+}
+
+function pilihMakanan1(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan1").style.display='block';
+    x = 21900
+    y = 421
+    z = 'Mie Ayam Teriyaki'
+    return x
+    return y
+    return z
+}
+function pilihMakanan2(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan2").style.display='block';
+    x = 17400
+    y = 350
+    z = 'Spaghetti Bolognaise'
+    return x
+    return y
+    return z
+}
+function pilihMakanan3(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan3").style.display='block';
+    x = 12800
+    y = 312
+    z = 'French Fries'
+    return x
+    return y
+    return z
+}
+function pilihMakanan4(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan4").style.display='block';
+    x = 31800
+    y = 639
+    z = 'Chicken Mozarella'
+    return x
+    return y
+    return z
+}
+function pilihMakanan5(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan5").style.display='block';
+    x = 30000
+    y = 239
+    z = 'Chicken Steak Chessy'
+    return x
+    return y
+    return z
+}
+function pilihMakanan6(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan6").style.display='block';
+    x = 37300
+    y = 200
+    z = 'Fish & Chips'
+    return x
+    return y
+    return z
+}
+function pilihMakanan7(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan7").style.display='block';
+    x = 23600
+    y = 680
+    z = 'Nasi Cap Cay'
+    return x
+    return y
+    return z
+}
+function pilihMakanan8(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan8").style.display='block';
+    x = 15500
+    y = 95
+    z = 'Soup Ayam'
+    return x
+    return y
+    return z
+}
+function pilihMakanan9(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan9").style.display='block';
+    x = 22800
+    y = 237
+    z = 'Presto Chicken Baked'
+    return x
+    return y
+    return z
+}
+function pilihMakanan10(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan10").style.display='block';
+    x = 27300
+    y = 412
+    z = 'Nasi Bistik Sapu'
+    return x
+    return y
+    return z
+}
+function pilihMakanan11(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan11").style.display='block';
+    x = 23600
+    y = 338
+    z = 'Nasi Ayam Cah Jamur'
+    return x
+    return y
+    return z
+}
+function pilihMakanan12(){
+    document.getElementById("dua").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMakanan12").style.display='block';
+    x = 26400
+    y = 604
+    z = 'Nasi Soup Tahu Seafood'
+    return x
+    return y
+    return z
+}
+function pilihMinuman1(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman1").style.display='block';
+    x = 8000
+    y = 45
+    z = 'Orange Juice'
+    return x
+    return y
+    return z
+}
+function pilihMinuman2(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman2").style.display='block';
+    x = 15000
+    y = 50
+    z = 'Black Currant'
+    return x
+    return y
+    return z
+}
+function pilihMinuman3(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman3").style.display='block';
+    x = 12000
+    y = 110
+    z = 'Frozen Capucino'
+    return x
+    return y
+    return z
+}
+function pilihMinuman4(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman4").style.display='block';
+    x = 18000
+    y = 180
+    z = 'Milkshake Strawbery'
+    return x
+    return y
+    return z
+}
+function pilihMinuman5(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman5").style.display='block';
+    x = 8000
+    y = 68
+    z = 'Es Lemon Tea'
+    return x
+    return y
+    return z
+}
+function pilihMinuman6(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman6").style.display='block';
+    x = 15000
+    y = 400
+    z = 'Bubble Tea'
+    return x
+    return y
+    return z
+}
+function pilihMinuman7(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman7").style.display='block';
+    x = 13000
+    y = 12
+    z = 'Ice Green Tea'
+    return x
+    return y
+    return z
+}
+function pilihMinuman8(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman8").style.display='block';
+    x = 18000
+    y = 100
+    z = 'Cheese Tea'
+    return x
+    return y
+    return z
+}
+function pilihMinuman9(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman9").style.display='block';
+    x = 18000
+    y = 83.4
+    z = 'Chocolate Milkshake'
+    return x
+    return y
+    return z
+}
+function pilihMinuman10(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman10").style.display='block';
+    x = 13000
+    y = 306
+    z = 'Dalgona Coffe'
+    return x
+    return y
+    return z
+}
+function pilihMinuman11(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman11").style.display='block';
+    x = 18000
+    y = 140
+    z = 'Tiramisu Ice Latte'
+    return x
+    return y
+    return z
+}
+function pilihMinuman12(){
+    document.getElementById("tiga").style.display='none';
+    document.getElementById("tombol").style.display='block';
+    document.getElementById("pilihMinuman12").style.display='block';
+    x = 13000
+    y = 364
+    z = 'Smoothie Mangga'
+    return x
+    return y
+    return z
+}
+harga = [];
+kalori = [];
+idMakanan = [];
+function tambahMenu(){
+    var name = document.getElementById("inputJumlah").value;
+    document.getElementById("inputJumlah").value='0';
+    hasil = x * name
+    kal = y * name
+    idM = z
+
+    idMakanan.push(idM)
+    harga.push(hasil)
+    kalori.push(kal)
+    alert(idM+' '+'Sebanyak'+' '+name+' '+'Berhasil Ditambahkan'+'\n'+'Harga : Rp.'+hasil+'\n'+'Kalori : '+kal+' '+'Kal'+'\n'+'Klik Batal Untuk Membatalkan')
+}
+function menuLain(){
+    document.getElementById("pilihMakanan1").style.display='none';
+    document.getElementById("pilihMakanan2").style.display='none';
+    document.getElementById("pilihMakanan3").style.display='none';
+    document.getElementById("pilihMakanan4").style.display='none';
+    document.getElementById("pilihMakanan5").style.display='none';
+    document.getElementById("pilihMakanan6").style.display='none';
+    document.getElementById("pilihMakanan7").style.display='none';
+    document.getElementById("pilihMakanan8").style.display='none';
+    document.getElementById("pilihMakanan9").style.display='none';
+    document.getElementById("pilihMakanan10").style.display='none';
+    document.getElementById("pilihMakanan11").style.display='none';
+    document.getElementById("pilihMakanan12").style.display='none';
+    document.getElementById("pilihMinuman1").style.display='none';
+    document.getElementById("pilihMinuman2").style.display='none';
+    document.getElementById("pilihMinuman3").style.display='none';
+    document.getElementById("pilihMinuman4").style.display='none';
+    document.getElementById("pilihMinuman5").style.display='none';
+    document.getElementById("pilihMinuman6").style.display='none';
+    document.getElementById("pilihMinuman7").style.display='none';
+    document.getElementById("pilihMinuman8").style.display='none';
+    document.getElementById("pilihMinuman9").style.display='none';
+    document.getElementById("pilihMinuman10").style.display='none';
+    document.getElementById("pilihMinuman11").style.display='none';
+    document.getElementById("pilihMinuman12").style.display='none';
+    document.getElementById("satu").style.display='block';
+    document.getElementById("tombol").style.display='none';
+}
+function batal(){
+    harga.pop()
+    kalori.pop()
+    idMakanan.pop()
+    alert('Menu Berhasil Dibatalkan')
+}
+function bayar(){
+    document.getElementById("pilihMakanan1").style.display='none';
+    document.getElementById("pilihMakanan2").style.display='none';
+    document.getElementById("pilihMakanan3").style.display='none';
+    document.getElementById("pilihMakanan4").style.display='none';
+    document.getElementById("pilihMakanan5").style.display='none';
+    document.getElementById("pilihMakanan6").style.display='none';
+    document.getElementById("pilihMakanan7").style.display='none';
+    document.getElementById("pilihMakanan8").style.display='none';
+    document.getElementById("pilihMakanan9").style.display='none';
+    document.getElementById("pilihMakanan10").style.display='none';
+    document.getElementById("pilihMakanan11").style.display='none';
+    document.getElementById("pilihMakanan12").style.display='none';
+    document.getElementById("pilihMinuman1").style.display='none';
+    document.getElementById("pilihMinuman2").style.display='none';
+    document.getElementById("pilihMinuman3").style.display='none';
+    document.getElementById("pilihMinuman4").style.display='none';
+    document.getElementById("pilihMinuman5").style.display='none';
+    document.getElementById("pilihMinuman6").style.display='none';
+    document.getElementById("pilihMinuman7").style.display='none';
+    document.getElementById("pilihMinuman8").style.display='none';
+    document.getElementById("pilihMinuman9").style.display='none';
+    document.getElementById("pilihMinuman10").style.display='none';
+    document.getElementById("pilihMinuman11").style.display='none';
+    document.getElementById("pilihMinuman12").style.display='none';
+    document.getElementById("tombol").style.display='none';
+    document.getElementById("keluaran").style.display='block';
+    var text = "";
+    var i;
+    for (i = 0; i < idMakanan.length; i++) {
+    text +='<tr><td scope="row">'+[i+1]+'</td><td>'+idMakanan[i]+'</td><td>'+kalori[i]+'</td><td>Rp.'+harga[i]+'</td></tr>';
+    document.getElementById("tampil").innerHTML = text;
+    }
+    document.getElementById("total").innerHTML = harga.reduce(myFunc);
+    document.getElementById("totalKal").innerHTML = kalori.reduce(myFunc);
+}
+function konfirmasi(){
+    document.getElementById("keluaran").style.display='none';
+    document.getElementById("jumlahBayar").style.display='block';
+    document.getElementById("bayar").innerHTML = harga.reduce(myFunc);
+}
+function batalkan(){
+    document.getElementById("keluaran").style.display='none';
+    document.getElementById("satu").style.display='block';
+    harga = [];
+    kalori = [];
+    idMakanan = [];
+}
+function ok(){
+    document.getElementById("jumlahBayar").style.display='none';
+    document.getElementById("satu").style.display='block';
+    harga = [];
+    kalori = [];
+    idMakanan = [];
+}
+function myFunc(total, num) {
+    return total + num;
+}
+<!--Tombol Tambah Kurang-->
+const inputJumlah = document.getElementById("inputJumlah");
+let inputJumlahValue = inputJumlah.value;
+function kurangJumlah() {
+    inputJumlah.value = --inputJumlahValue;
+}
+function tambahJumlah() {
+    inputJumlah.value = ++inputJumlahValue;
+}
